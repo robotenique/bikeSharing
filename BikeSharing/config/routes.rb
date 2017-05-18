d53cadb172f53129391776622d496d522315e121
Claudio Silva <ddk.claudio@gmail.com>
2017-05-18 01:18:15 +0000

    Added controller Api & new Home

--- BikeSharing/config/routes.rb
+++ BikeSharing/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'api/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root_url
+  root 'api#index'
 
 end
