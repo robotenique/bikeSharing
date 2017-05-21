85a8f029e661940c8f6fdf04520de565d39c63bd
Caio R BCarmo <caio.bedulli@gmail.com>
2017-05-21 19:55:57 -0300

    Position is now destroyed too

--- BikeSharing/app/controllers/stations_controller.rb
+++ BikeSharing/app/controllers/stations_controller.rb
@@ -61,6 +61,7 @@ class StationsController < ApplicationController
   # DELETE /stations/1
   # DELETE /stations/1.json
   def destroy
+    Position.find(@station.id).destroy!
     @station.destroy
     respond_to do |format|
       format.html { redirect_to stations_url, notice: 'Station was successfully destroyed.' }
