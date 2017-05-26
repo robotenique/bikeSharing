f3c3cc3c7a68f0c09f22c4b3c93598a0fead16d5
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2017-05-26 14:26:56 -0300

    Adds get user location from cookie functionality
    Signed-off-by: Bruno B. Scholl <bruno.scholl@usp.br>
    Signed-off-by: Juliano Garica <robotenique@linux.ime.usp.br>

--- BikeSharing/app/controllers/api_controller.rb
+++ BikeSharing/app/controllers/api_controller.rb
@@ -1,6 +1,15 @@
 class ApiController < ApplicationController
-  layout 'map', :only => [ :index, :show ]
-  def index
-  end
+    layout 'map', :only => [ :index, :show ]
+    def index
+        @lat_lng = nil;
+    end
+    def action
+        if user_signed_in?
+            @user = current_user
+            @lat_lng = cookies[:lat_lng].split("|")
+            @user.latitude = @lat_lng[0]
+            @user.longitude = @lat_lng[1]
+        end
+    end
 
 end
