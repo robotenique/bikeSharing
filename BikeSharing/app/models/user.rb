fd5312f089f303d67b05ea13c2fcfa739a9418d6
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2017-05-26 14:13:33 -0300

    Adds location and geolocation to user model.
    Signed-off-by: Bruno B. Scholl <bruno.scholl@usp.br>
    Signed-off-by: Juliano Garcia <robotenique@linux.ime.usp.br>

--- BikeSharing/app/models/user.rb
+++ BikeSharing/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ reverse_geocoded_by :latitude, :longitude
+ after_validation :reverse_geocode
+  attr_accessor :latitude, :longitude, :address
 end
