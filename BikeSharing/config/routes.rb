d6256ecce797b9975d256c3bca03839fc53e1d76
Bruno Scholl <bruno.scholl@usp.br>
2017-06-18 20:42:10 -0300

    Change route from free_slots to free_slots_bikes
    This change allows to search for either a free bike or slot whith only one route.
    
    Signed-off-by: Bruno Scholl <bruno.scholl@usp.br>
    Signed-off-by: Juliano Garcia <robotenique@linux.ime.usp.br>

--- BikeSharing/config/routes.rb
+++ BikeSharing/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :positions
   resources :stations
   get 'api/index'
-  get 'api/free_slots'
+  get 'api/free_slots_bikes'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
