36deee1e80e834f4d692f012e1a45518cab0c462
Bruno Scholl <bruno.scholl@usp.br>
2017-06-18 21:25:41 -0300

    Add schema updated
    Signed-off-by: Bruno Scholl <bruno.scholl@usp.br>

--- BikeSharing/db/schema.rb
+++ BikeSharing/db/schema.rb
@@ -15,13 +15,10 @@ ActiveRecord::Schema.define(version: 20170616204504) do
   create_table "stations", force: :cascade do |t|
     t.string "company"
     t.integer "free_bikes"
-    t.integer "empty_slots"
     t.integer "slots"
     t.string "name"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "citybik_station_id"
-    t.string "citybik_station_uid"
+    t.float "lat"
+    t.float "long"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -39,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170616204504) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "lar"
+    t.float "lon"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
