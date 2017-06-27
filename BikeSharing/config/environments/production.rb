e11827690ef62f592bbc81847719240606b267fc
robotenique <julianogarcia_1997@hotmail.com>
2017-06-27 16:39:26 -0300

    modify the production initializer to work properly with heroku

--- BikeSharing/config/environments/production.rb
+++ BikeSharing/config/environments/production.rb
@@ -28,10 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
-
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
