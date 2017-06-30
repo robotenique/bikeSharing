62b1702585acdb040775ecefae19c39e8af1dc3d
Claudio Silva <ddk@localhost.localdomain>
2017-06-30 15:43:52 -0300

    Revert "Signed-off-by: Claudio Silva <ddk.claudio@gmail.com>"
    This reverts commit b6982dab03f68e09f4d2068eb389f8ae3be95e81.

--- BikeSharing/db/migrate/20170629203047_add_open_time_to_stations.rb
+++
@@ -1,6 +0,0 @@
-class AddOpenTimeToStations < ActiveRecord::Migration[5.1]
-  def change
-    add_column :stations, :open_time, :datetime
-    add_column :stations, :close_time, :datetime
-  end
-end
