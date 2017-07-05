1d56c69f85a5fcdc44a3d744cec0b36fce3f33fd
Caio R BCarmo <caio.bedulli@gmail.com>
2017-07-05 19:36:02 -0300

    Added validation so user cannot bookmark twice same station
    Signed-off-by: Caio Carmo <caio.bedulli@gmail.com>
    Signed-off-by: Duílio Elias <duilioelias@gmail.com>

--- BikeSharing/app/models/bookmark_station.rb
+++ BikeSharing/app/models/bookmark_station.rb
@@ -1,4 +1,15 @@
+class AlreadyBookmarked < ActiveModel::Validator
+  def validate(record)
+    user = record.user.id
+    station = record.station.id
+    if BookmarkStation.find_by(station_id: station, user_id: user)
+      record.errors[:base] << "Already bookmarked."
+    end
+  end
+end
+
 class BookmarkStation < ApplicationRecord
   belongs_to :user
   belongs_to :station
+  validates_with AlreadyBookmarked
 end
