789655844b44487a1dad5e832ce5695ca62840a5
Caio R BCarmo <caio.bedulli@gmail.com>
2017-07-05 18:59:59 -0300

    Typo
    Signed-off-by: Caio Carmo <caio.bedulli@gmail.com>
    Signed-off-by: Duílio Elias <duilioelias@gmail.com>

--- BikeSharing/app/ranks_rules/abstract_rank.rb
+++ BikeSharing/app/ranks_rules/abstract_rank.rb
@@ -1,5 +1,5 @@
 class AbstractRank
-  # Child classes must return a sorted list of pairs (station_id, value). Order in desc way
+  # Child classes must return a sorted list of pairs (station_id, value). Sorted in desc way
   def get_rank
     return nil
   end
