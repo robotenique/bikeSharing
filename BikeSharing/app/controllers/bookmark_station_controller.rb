fc3c6c0ae2c9a88ce1e6ba9a2bafe1e396d6bd76
Caio R BCarmo <caio.bedulli@gmail.com>
2017-07-05 22:22:00 -0300

    implemented unbookmark station, implemented more validations
    Signed-off-by: Caio Carmo <caio.bedulli@gmail.com>
    Signed-off-by: Duílio Elias <duilioelias@gmail.com>

--- BikeSharing/app/controllers/bookmark_station_controller.rb
+++ BikeSharing/app/controllers/bookmark_station_controller.rb
@@ -1,10 +1,30 @@
 class BookmarkStationController < ApplicationController
   def unbookmark
+    # if !user_signed_in?
+    #   puts "NAO TEM NINGUEM AQUI"
+    # end
+    station = Station.find_by(name: params[:station_name])
+    user    = User.find(current_user.id)
+    if station == nil
+      @ret_message = "Station not found"
+      return
+    end
+    bookmarked_station = BookmarkStation.find_by(user: user, station: station)
+    if bookmarked_station == nil
+      @ret_message = "Not bookmarked yet"
+      return
+    end
+    bookmarked_station.delete
+    @ret_message = "Unbookmarked =/"
   end
 
   def bookmark
     station = Station.find_by(name: params[:station_name])
     user    = User.find(current_user.id)
+    if station == nil
+      @ret_message = "Station not found"
+      return
+    end
     if BookmarkStation.find_by(user: user, station: station)
       @ret_message = "Already bookmarked"
       return
